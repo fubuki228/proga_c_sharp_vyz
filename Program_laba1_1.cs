a777b5dd5bcb62d3dde077b988c457764698c9af
fubuki228 <119005316+fubuki228@users.noreply.github.com>
2022-12-04 22:23:04 +0300

    Add files via upload

---
+++ Program_laba1_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Short\tmin: {short.MinValue}\tmax: {short.MaxValue} {Environment.NewLine}" +
+                              $"Unsigned Short\tmin: {ushort.MinValue}\tmax: {ushort.MaxValue} {Environment.NewLine}" +
+                              $"Integer\tmin: {int.MinValue}\tmax: {int.MaxValue} {Environment.NewLine}" +
+                              $"Unsigned integer\tmin: {uint.MinValue}\tmax: {uint.MaxValue} {Environment.NewLine}" +
+                              $"Long\tmin: {long.MinValue}\tmax: {long.MaxValue} {Environment.NewLine}" +
+                              $"Unsigned long\tmin: {ulong.MinValue}\tmax: {ulong.MaxValue} {Environment.NewLine}" +
+                              $"Signed Byte\tmin: {sbyte.MinValue}\tmax: {sbyte.MaxValue} {Environment.NewLine}" +
+                              $"Byte\tmin: {byte.MinValue}\tmax: {byte.MaxValue} {Environment.NewLine}" +
+                              $"Float point\tmin: {float.MinValue}\tmax: {float.MaxValue} {Environment.NewLine}");
+        }
+    }
+}
+//Выведите на консоль минимальные и максимальные значения для предопределенных типов данных CTS.
